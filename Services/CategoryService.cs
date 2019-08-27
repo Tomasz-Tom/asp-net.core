a2bbea89b99c703196faad8347a1d8b6659d5b80
SWITALA Tomasz <tomasz.switala@ext.soprasteria.com>
2019-08-27 16:36:43 +0200

    api

---
+++ Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Domain.Services;
+
+namespace Supermarket.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRespository _categoryRespository;
+
+        public CategoryService(ICategoryRespository categoryRespository)
+        {
+            this._categoryRespository = categoryRespository;
+        }
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _categoryRespository.ListAsync();
+        }
+    }
+}
